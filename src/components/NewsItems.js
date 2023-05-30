449facc84411b8f6a1bd1305991b9b2c39babc30
ShetSahil <20104122.sahilshetty@gmail.com>
2023-05-30 23:46:50 +0530

    adding previous and next button for manipulating the content of pages

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -7,11 +7,11 @@ export class NewsItems extends Component {
     return (
     <div className="my-3">
     <div className="card" style={{width: "18rem"}}>
-        <img src={imageUrl} className="card-img-top" alt="..."/>
+        <img src={!imageUrl ? "https://www.deccanherald.com/sites/dh/files/articleimages/2023/05/29/file7qdtzpo4f88fn3753xv-1-1222897-1685356452.jpg":imageUrl }className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}...</h5>
           <p className="card-text">{description}...</p>
-          <a href={newsUrl} target="_blank"className="btn brn-sm  btn-primary">Read More</a>
+          <a href={newsUrl} target="_blank"className="btn brn-sm  btn-dark">Read More</a>
         </div>
     </div>
     </div>
