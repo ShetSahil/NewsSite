afe0b534abdad9e5d7b694ee77fc9a21fa835509
ShetSahil <20104122.sahilshetty@gmail.com>
2023-05-30 20:46:02 +0530

    calling contens descrptions and all through articles state

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 export class NewsItems extends Component {
 
   render() {
-    let{title,description,imageUrl,keyUrl} = this.props;
+    let{title,description,imageUrl,newsUrl} = this.props;
     return (
     <div className="my-3">
     <div className="card" style={{width: "18rem"}}>
         <img src={imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{description}</p>
-          <a href="/newsdetail" className="btn brn-sm  btn-primary">Read More</a>
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <a href={newsUrl} target="_blank"className="btn brn-sm  btn-primary">Read More</a>
         </div>
     </div>
     </div>
